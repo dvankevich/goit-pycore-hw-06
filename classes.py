e5e51257777d4532e52cd91daa6a492cfefa0efc
dvankevich <dvankevich@gmail.com>
2025-03-26 10:58:01 +0200

    add del_phone method

--- classes.py
+++ classes.py
@@ -9,12 +9,12 @@ class Field:
 
 class Name(Field):
     def __init__(self, value):
-        # add check is value not empty
+        # ToDo add check is value not empty
         super().__init__(value)
 
 class Phone(Field):
     def __init__(self, value):
-        # add pnone number validation
+        # ToDo add pnone number validation
         super().__init__(value)
 
 class Record:
@@ -22,11 +22,11 @@ class Record:
         self.name = Name(name)
         self.phones = []
 
-    def add_phone(self, phoneNumber):
-        self.phones.append(Phone(phoneNumber))
+    def add_phone(self, number):
+        self.phones.append(Phone(number))
 
-    def del_phone(self, phoneNumber):
-        self.phones.remove[Phone(phoneNumber)]
+    def del_phone(self, number):
+        self.phones = [phone for phone in self.phones if phone.value != number]
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
@@ -39,6 +39,20 @@ if __name__ == "__main__":
     record01 = Record("Name01")
     print(record01.name, len(record01.phones))
     assert str(record01.name) == "Name01"
+
     record01.add_phone("1234567890")
     record01.add_phone("1234567891")
+    print(record01, len(record01.phones))
+    assert len(record01.phones) == 2
+
+    record01.del_phone("1234567891")
+    assert len(record01.phones) == 1
+
+    record01.add_phone("1234567891")
+    record01.add_phone("1234567891")
+    assert len(record01.phones) == 3
+
+    record01.del_phone("1234567891")
+    assert len(record01.phones) == 1
+    
     print(record01, len(record01.phones))
